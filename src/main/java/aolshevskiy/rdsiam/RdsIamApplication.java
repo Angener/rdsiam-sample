05e36ad8d7f7d22e689eb508fecea233e6f05d16
Artyom Olshevskiy <artyom.olshevskiy@gmail.com>
2021-01-24 12:52:18 +0200

    Prevent metadata fetch exception

--- src/main/java/aolshevskiy/rdsiam/RdsIamApplication.java
+++ src/main/java/aolshevskiy/rdsiam/RdsIamApplication.java
@@ -6,8 +6,10 @@ import com.amazonaws.services.rds.auth.RdsIamAuthTokenGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 import org.springframework.cloud.aws.core.region.RegionProvider;
 import org.springframework.context.annotation.Bean;
 
@@ -16,6 +18,7 @@ import java.net.URI;
 import java.sql.SQLException;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = ContextInstanceDataAutoConfiguration.class)
 public class RdsIamApplication {
     private static final Logger logger = LoggerFactory.getLogger(RdsIamApplication.class);
 
